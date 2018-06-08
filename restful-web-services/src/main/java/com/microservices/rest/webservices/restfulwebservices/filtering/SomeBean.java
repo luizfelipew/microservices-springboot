ea624a3dd401ef3d653ae38b7a3580ddd141d068
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-06-08 14:52:32 -0300

    Implementando filter

--- restful-web-services/src/main/java/com/microservices/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/microservices/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,10 +1,9 @@
 package com.microservices.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties(value = {"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
