bb36d00e863bd4d6c089b7058dd2079617dc1ad7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-08-08 15:35:03 -0300

    Get methods with JPA

--- restful-web-services/src/main/java/com/microservices/rest/webservices/restfulwebservices/SecurityConfig.java
+++ restful-web-services/src/main/java/com/microservices/rest/webservices/restfulwebservices/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.microservices.rest.webservices.restfulwebservices;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
+/*@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
     }
-}
+}*/
