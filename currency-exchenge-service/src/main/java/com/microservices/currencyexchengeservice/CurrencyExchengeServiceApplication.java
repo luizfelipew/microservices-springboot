1f336112c7e65b8ba19948ec0625c08908fbf06c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-02-22 13:47:11 -0300

    Setting up Currency Exchange Microservices

---
+++ currency-exchenge-service/src/main/java/com/microservices/currencyexchengeservice/CurrencyExchengeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.currencyexchengeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchengeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchengeServiceApplication.class, args);
+	}
+
+}
